1273c3b17b33e8cd1651f74686ee2ccec13f983c
unknown <>
2016-04-28 21:07:29 +0800

    Qu2.py

---
+++ Qu2.py
@@ -0,0 +1,10 @@
+Queue = [] #using list as a queue
+
+Queue.insert(0,'shiang') #enqueue of Queue
+print(Queue)
+Queue.insert(0,333) #enqueue of Queue
+print(Queue)
+size = len(Queue) #size of Queue
+print(size)
+Queue.pop() #dequeue of Queue
+print(Queue)
