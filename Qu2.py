0596c38cbf97be869e91884ae990bb84300a547e
legend-shiang <shark340@gmail.com>
2016-04-29 20:03:56 +0800

    Update Qu2.py

--- Qu2.py
+++ Qu2.py
@@ -1,3 +1,19 @@
+class Queue:
+    def __init__(self):
+        self.items = []
+
+    def isEmpty(self):
+        return self.items == []
+
+    def enqueue(self, item):
+        return self.items.append(item)
+
+    def dequeue(self):
+        return self.items.pop(0)
+
+    def size(self):
+        return len(self.items)
+        
 Queue = [] #using list as a queue
 
 Queue.insert(0,'shiang') #enqueue of Queue
